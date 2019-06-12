dd17d14eadee61d14a0f9d13a0357579f8a9910d
luszczeksebastian@gmail.com <luszczeksebastian@gmail.com>
2019-06-12 15:33:04 +0200

    +mongoDB

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+// env variables from .env file recognized
+const dotenv = require("dotenv");
+dotenv.config();
+
+// import models
+const Event = require("./models/event");
+const User = require("./models/user");
 
 const app = express();
 
@@ -9,55 +19,137 @@ const events = [];
 
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHttp({
+app.use(
+  "/graphql",
+  graphqlHttp({
     schema: buildSchema(`
         type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+          creator: String!
+        }
+
+        type User {
+          _id: ID!
+          email: String!
+          password: String
         }
 
         input EventInput {
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+
+        input UserInput {
+          email: String!
+          password: String!
         }
 
         type RootQuery {
-            events: [Event!]!
+          events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(eventInput: EventInput): Event
+          createEvent(eventInput: EventInput): Event
+          createUser(userInput: UserInput): User
         }
 
         schema {
-            query: RootQuery
-            mutation: RootMutation
+          query: RootQuery
+          mutation: RootMutation
         }
     `),
     rootValue: {
-        events: () => {
-            return events;
-        },
-        createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: args.eventInput.price,
-                date: args.eventInput.date
+      events: () => {
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
+      },
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: args.eventInput.price,
+          date: new Date(args.eventInput.date),
+          creator: "5d00facf8dd2d118bf6c9f64"
+        });
+        let createdEvent;
+        return event
+          .save()
+          .then(result => {
+            createdEvent = { ...result._doc };
+            return User.findById("5d00facf8dd2d118bf6c9f64");
+          })
+          .then(user => {
+            if (!user) {
+              throw new Error("User doesn't exist!");
             }
-            events.push(event);
-            return event;
-        }
-    }, 
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then(result => {
+            return createdEvent;
+          })
+          .catch(err => {
+            console.error(err);
+            throw err;
+          });
+        return event;
+      },
+      createUser: args => {
+        return User.findOne({
+          email: args.userInput.email
+        })
+          .then(user => {
+            if (user) {
+              throw new Error("User already exist!");
+            }
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then(hashedPassword => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword
+            });
+            return user.save();
+          })
+          .then(result => {
+            return { ...result._doc, password: null };
+          })
+          .catch(err => {
+            throw err;
+          });
+      }
+    },
     graphiql: true
-}))
+  })
+);
 
-app.listen(3000, () => {
-    console.log('Server started...')
-})
+mongoose
+  .connect(
+    `mongodb+srv://utkow1991:${
+      process.env.MONGO_PASS
+    }@cluster0-ympqd.mongodb.net/${
+      process.env.MONGO_DB
+    }?retryWrites=true&w=majority`,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server started with db...");
+    });
+  })
+  .catch(err => {
+    console.error(err);
+  });
